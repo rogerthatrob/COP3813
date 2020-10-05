4e2fcec55de71890feeacf6716ea21da0d6935dd
Robert Haire <rogerthatrob@gmail.com>
2020-10-05 19:36:10 -0400

    testing js

---
+++ Sandbox/test.js
@@ -0,0 +1,7 @@
+var number1 = 6; // Code tested with values: 6 and 4
+var number2 = 2; // Code tested with values: 2 and -2
+
+var finalValue = 0;
+finalValue = number1 * number2
+debugger
+console.log(finalValue)
