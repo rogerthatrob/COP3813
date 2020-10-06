53ca0c11f9adfa633740bfe383ee831abf09e1b6
Robert Haire <rogerthatrob@gmail.com>
2020-10-06 15:50:12 -0400

    complete basics of P3

---
+++ P3/p3.js
@@ -0,0 +1,28 @@
+//max
+function myMax(){
+	a = document.getElementById("n1").value
+  b = document.getElementById("n2").value
+  c = document.getElementById("n3").value
+  temp = Math.max(a,b,c)
+  console.log(temp)
+  alert("the max of three number is "+temp)
+  //return(temp)
+  
+}
+// min
+function myMin(){
+	a = document.getElementById("n1").value
+  b = document.getElementById("n2").value
+  c = document.getElementById("n3").value
+  temp = Math.min(a,b,c)
+  console.log(temp)
+  alert("the min of three number is "+temp)
+  //return(temp)
+  
+}
+
+// average
+
+// median
+
+// range (max-min)
