f96413411789cd1360ad2adbb44962c4afca2e09
Robert Haire <rogerthatrob@gmail.com>
2020-10-08 14:39:45 -0400

    had to parseInt to convert the strings to ints. could do parseFloat later if i have time

--- P3/startbootstrap-new-age-master/js/p3.js
+++ P3/startbootstrap-new-age-master/js/p3.js
@@ -3,9 +3,9 @@ function myMax(){
 	a = document.getElementById("n1").value
   b = document.getElementById("n2").value
   c = document.getElementById("n3").value
-  temp = Math.max(a,b,c)
-  console.log(temp)
-  alert("the max of three number is "+temp)
+  maxNum = Math.max(a,b,c)
+  console.log(maxNum)
+  alert("the max of three numbers is " +maxNum)
   //return(temp)
   
 }
@@ -14,15 +14,34 @@ function myMin(){
 	a = document.getElementById("n1").value
   b = document.getElementById("n2").value
   c = document.getElementById("n3").value
-  temp = Math.min(a,b,c)
-  console.log(temp)
-  alert("the min of three number is "+temp)
+  minNum = Math.min(a,b,c)
+  console.log(minNum)
+  alert("the min of three numbers is " +minNum)
   //return(temp)
   
 }
 
 // average
-
+function myAverage(){
+  a = parseInt(document.getElementById("n1").value)
+  b = parseInt(document.getElementById("n2").value)
+  c = parseInt(document.getElementById("n3").value)
+  averageNum = (a+b+c)/3
+  console.log(averageNum)
+  alert("the average of three numbers is " +averageNum)
+}
 // median
-
+function myMedian(){
+  a = parseInt(document.getElementById("n1").value)
+  b = parseInt(document.getElementById("n2").value)
+  c = parseInt(document.getElementById("n3").value)
+  if(a>b && b<c){
+    alert("median is: " +b)
+  } elseif(b>a && a<c);{
+    alert("median is: " +a)
+  } elseif(a>c && c<b);{
+    alert("median is: " +c)
+  }
+  
+}
 // range (max-min)
